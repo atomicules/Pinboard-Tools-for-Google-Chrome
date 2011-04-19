4f56b6866390418e3218ec3ca2e1aa0bb52167c1
i5m <i5ivem@gmail.com>
2011-04-19 13:35:06 +0100

    Implement the Save Tab Set feature
    Pinboard.In announced a Save Tab Set feature
    (http://pinboard.in/resources/safari/save_tabs/). This commit just ports
    the functionality provided in the Safari Extension to this Chrome extension
    
    Still to do is check for chrome:// and about:, etc tabs and remove these
    from the list.
    
    - .crx - repackaged the extension with these changes.
    - addPinboard.js - Mostly just port the code across from the Safari
      extension. Has to be implemented slightly different as Chrome does
      everything as callbacks.
    - manifest.json - Incremented version and removed key and update url. The
      original fork is no longer published as a extension and this info is not
      relevent to this fork.
    - popup.html - Add new menu item for saving tab sets.
    - README - Altered this to reflect new version number (1.3)

--- Pinboard-Tools-for-Google-Chrome/addPinboard.js
+++ Pinboard-Tools-for-Google-Chrome/addPinboard.js
@@ -1,4 +1,5 @@
-var pinboardUrl = "http://pinboard.in/add?";
+var BASE_URL = 'https://pinboard.in';
+var pinboardUrl = BASE_URL+"/add?";
 var url;
 var title;
 function add_bookmark() {
@@ -26,3 +27,73 @@ function add_later() {
  		window.open(pinboardUrl + 'later=yes&noui=yes&jump=close&url=' + encodeURIComponent(url) + '&title=' + encodeURIComponent(title) + ' ','Pinboard','toolbar=no,width=100,height=100');
 	});
 }
+
+//Below based on Pinboard.In Safari extension
+//https://pinboard.in/resources/safari/save_tabs/
+var SUBMIT_URL  = BASE_URL + '/tabs/save/';
+var DISPLAY_URL = BASE_URL + '/tabs/show/';
+
+function save_tabs() {
+	chrome.windows.getAll({"populate" : true}, makeTabList);
+}
+
+/*
+// Not doing this yet, but could do with removing any chrome://, about:, etc tabs
+// check if there are any nonempty tabs open
+function hasContentTabs()
+{
+    var winz = safari.application.browserWindows;
+    
+    for (var i = 0; i < winz.length; i++)
+    {
+        var tabz = winz[i].tabs;
+        for (var j = 0; j < tabz.length; j++)
+        {
+            if (tabz[j].url)
+            {
+                return true;
+            }
+        }
+    }   
+    return false;
+}*/
+
+//Callback thingyamabob based on Chromium Developer Examples
+//http://src.chromium.org/viewvc/chrome/trunk/src/chrome/common/extensions/docs/examples/api/windows/merge_windows/background.html?revision=39116&view=markup
+
+function makeTabList(windows) {
+	winz = [];
+	result = { browser: "chrome",  windows: winz }; 
+
+	var chromeWinz = windows;
+
+	for (var i = 0; i < chromeWinz.length; i++) {
+		var chromeTabz = chromeWinz[i].tabs;
+		var tabz = [];
+
+		for (var j = 0; j < chromeTabz.length; j++) {
+			var cTab = chromeTabz[j];
+			if (cTab.url) {
+				tabz.push({	title:   cTab.title, 
+											url:     cTab.url }); 
+			}
+		}
+		winz.push(tabz);
+	}   
+	console.log(result);
+	tabList = result;
+
+	var params = new FormData();
+	var req   = new XMLHttpRequest(); 
+
+	params.append("data", JSON.stringify(result));
+	req.open("POST", SUBMIT_URL, true);        
+
+	req.onreadystatechange = function() {
+	  if (req.readyState == 4) {
+			chrome.tabs.create({url:DISPLAY_URL});
+		}
+  }   
+	req.send(params);
+}
+
